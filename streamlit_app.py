060bb5ea8f9590df3d79b3f49bdb950e3277f1a7
mikrutm <daneanalzialokalnie@gmail.com>
2024-05-16 16:41:44 +0200

    dfsd

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,7 +106,7 @@ st.sidebar.title("Lista plików tekstowych")
 
 
 # Sortowanie przefiltrowanej listy plików według daty wyodrębnionej z nazw plików
-#txt_files = sorted(txt_files, key=extract_date_from_filename)
+txt_files = sorted(txt_files, key=extract_date_from_filename)
 
 with st.sidebar:
 
